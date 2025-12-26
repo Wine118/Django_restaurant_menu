396a164c3fd4de100d44cfc1ef9cef7d5a9aa23c
Wine118 <winewaitwaiphyo@gmail.com>
2025-12-26 12:08:15 +0630

    connecting complaints database to admin dashboard

--- cafe_dashboard/views.py
+++ cafe_dashboard/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render 
+from django.core.paginator import Paginator 
+from complaints.models import CustomerComplaint
+from datetime import datetime
 from django.contrib.auth.decorators import login_required  
 
 # Create your views here.
 @login_required
 def dashboard(request):
-    return render(request, 'cafe_dashboard/admin.html')
+    complaints = CustomerComplaint.objects.all().order_by('-created_at')
+    
+    # Date filter (from calendar input)
+    selected_date = request.GET.get('date')
+    if selected_date:
+        complaints = complaints.filter(
+            created_at__date = selected_date
+        )
+
+    # Pagination (20 per page)
+    paginator = Paginator(complaints, 20)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    
+
+    context = {
+        'complaints' : page_obj,
+        'selected_date' : selected_date
+    }
+    return render(request, 'cafe_dashboard/admin.html',context)
